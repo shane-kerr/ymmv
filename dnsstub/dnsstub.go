88ed5f5a0d1d52a5ff97aa76646a59967eafb33d
Shane Kerr <shane@time-travellers.org>
2016-06-02 14:16:29 +0800

    Export the DNS query method. Add time as a tracked result. Allow specifying resolver on init (instead of reading from /etc/resolv.conf)

--- dnsstub/dnsstub.go
+++ dnsstub/dnsstub.go
@@ -1,10 +1,11 @@
 package dnsstub
-//package main
 
 import (
 	"math/big"
 	"crypto/rand"
 	"fmt"
+	"net"
+	"time"
 	"github.com/miekg/dns"
 )
 
@@ -19,11 +20,11 @@ type answer struct {
 	qname	string
 	rtype	uint16
 	answer	*dns.Msg
+	rtt	time.Duration
 	err	error
 }
 
 type StubResolver struct {
-	resolv_conf		*dns.ClientConfig
 	next_handle		int
 	queries			chan *query
 	answers			chan *answer
@@ -40,32 +41,35 @@ func RandUint16() (uint16, error) {
 	return uint16(id.Uint64()), nil
 }
 
-func query_resolver(resolver string, query *dns.Msg) (*dns.Msg, error) {
+/*
+   Send a query to a DNS server, retrying and handling truncation.
+ */
+func DnsQuery(server string, query *dns.Msg) (*dns.Msg, time.Duration, error) {
 	// try to query first in UDP
 	dnsClient := new(dns.Client)
 	id, err := RandUint16()
 	if err != nil {
-		return nil, err
+		return nil, 0, err
 	}
 	query.Id = id
-	r, _, err := dnsClient.Exchange(query, resolver)
+	r, rtt, err := dnsClient.Exchange(query, server)
 	if err != nil {
-		return nil, err
+		return nil, 0, err
 	}
 	if (r.Rcode == dns.RcodeSuccess) && !r.Truncated {
-		return r, nil
+		return r, rtt, nil
 	}
 	// if this didn't work, try again in TCP
 	dnsClient.Net = "tcp"
-	r, _, err = dnsClient.Exchange(query, resolver)
+	r, rtt, err = dnsClient.Exchange(query, server)
 	if err != nil {
-		return nil, err
+		return nil, 0, err
 	}
 	// return whatever we get in this case, even if an erroneous response
-	return r, nil
+	return r, rtt, nil
 }
 
-func stub_resolve(resolv_conf *dns.ClientConfig, queries <-chan *query, answers chan<- *answer) {
+func stub_resolve(servers []string, queries <-chan *query, answers chan<- *answer) {
 	for q := range queries {
 		dns_query := new(dns.Msg)
 		dns_query.RecursionDesired = true
@@ -75,9 +79,9 @@ func stub_resolve(resolv_conf *dns.ClientConfig, queries <-chan *query, answers 
 		a.qname = q.qname
 		a.rtype = q.rtype
 		a.answer = nil
-		for _, server := range resolv_conf.Servers {
+		for _, server := range servers {
 			resolver := server + ":53"
-			a.answer, a.err = query_resolver(resolver, dns_query)
+			a.answer, a.rtt, a.err = DnsQuery(resolver, dns_query)
 			if a.answer != nil {
 				break
 			}
@@ -86,17 +90,24 @@ func stub_resolve(resolv_conf *dns.ClientConfig, queries <-chan *query, answers 
 	}
 }
 
-func Init(concurrency int) (resolver *StubResolver, err error) {
+func Init(concurrency int, server_ips []net.IP) (resolver *StubResolver, err error) {
 	stub := new(StubResolver)
-	stub.resolv_conf, err = dns.ClientConfigFromFile("/etc/resolv.conf")
-	if err != nil {
-		newerr := fmt.Errorf("error reading resolver configuration from '/etc/resolv.conf'; %s", err)
-		return nil, newerr
+	servers := make([]string, 0, 0)
+	for _, ip := range server_ips {
+		servers = append(servers, ip.String())
+	}
+	if len(servers) == 0 {
+		resolv_conf, err := dns.ClientConfigFromFile("/etc/resolv.conf")
+		if err != nil {
+			newerr := fmt.Errorf("error reading resolver configuration from '/etc/resolv.conf'; %s", err)
+			return nil, newerr
+		}
+		servers = resolv_conf.Servers
 	}
 	stub.queries = make(chan *query, concurrency * 4)
 	stub.answers = make(chan *answer, concurrency * 2)
 	for i := 0; i < concurrency; i++ {
-		go stub_resolve(stub.resolv_conf, stub.queries, stub.answers)
+		go stub_resolve(servers, stub.queries, stub.answers)
 	}
 	return stub, nil
 }
@@ -111,7 +122,7 @@ func (resolver *StubResolver) Query(qname string, rtype uint16) (handle int) {
 	return q.handle
 }
 
-func (resolver *StubResolver) Wait() (*dns.Msg, string, uint16, error) {
+func (resolver *StubResolver) Wait() (*dns.Msg, time.Duration, string, uint16, error) {
 	var a *answer
 	// if we have waiting finished answers, return one of them
 	if len(resolver.finished_answers) > 0 {
@@ -121,21 +132,21 @@ func (resolver *StubResolver) Wait() (*dns.Msg, string, uint16, error) {
 	} else {
 		a = <-resolver.answers
 	}
-	return a.answer, a.qname, a.rtype, a.err
+	return a.answer, a.rtt, a.qname, a.rtype, a.err
 }
 
-func (resolver *StubResolver) WaitByHandle(handle int) (*dns.Msg, string, uint16, error) {
+func (resolver *StubResolver) WaitByHandle(handle int) (*dns.Msg, time.Duration, string, uint16, error) {
 	// check any existing finished answers to see if we have ours
 	for n, a := range resolver.finished_answers {
 		if a.handle == handle {
 			resolver.finished_answers = append(resolver.finished_answers[:n], resolver.finished_answers[n+1:]...)
-			return a.answer, a.qname, a.rtype, a.err
+			return a.answer, a.rtt, a.qname, a.rtype, a.err
 		}
 	}
 	for {
 		a := <-resolver.answers
 		if a.handle == handle {
-			return a.answer, a.qname, a.rtype, a.err
+			return a.answer, a.rtt, a.qname, a.rtype, a.err
 		}
 		resolver.finished_answers = append(resolver.finished_answers, a)
 	}
@@ -146,18 +157,20 @@ func (resolver *StubResolver) Close() {
 	close(resolver.answers)
 }
 
-/* 
+/*
 func main() {
-	resolver, err := Init(11)
+	resolver, err := Init(11, nil)
 	if err != nil {
 		fmt.Printf("Error! %s\n", err)
 		return
 	}
 	resolver.Query("isc.org.", dns.TypeA)
+	sleep_time, _ := time.ParseDuration("1s")
+	time.Sleep(sleep_time)	// insure that our non-handle query finishes first
 	handle := resolver.Query("isc.org.", dns.TypeAAAA)
-	answer, err := resolver.WaitByHandle(handle)
+	answer, _, _, err := resolver.WaitByHandle(handle)
 	fmt.Printf("answer: %s\n", answer)
-	answer, err = resolver.Wait()
+	answer, _, _, err = resolver.Wait()
 	fmt.Printf("answer: %s\n", answer)
 	resolver.Close()
 }
