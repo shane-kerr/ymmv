1c4bf1824559fe5fddaab0fda6bd8cbd471b7ed8
Shane Kerr <shane@time-travellers.org>
2016-10-03 21:34:59 +0200

    Split managing list of root servers into separate file. Support for query obfuscation.

--- ymmv/ymmv.go
+++ ymmv/ymmv.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"crypto/sha256"
 	"encoding/binary"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"github.com/miekg/dns"
@@ -15,7 +17,6 @@ import (
 	"sort"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 )
 
@@ -29,30 +30,6 @@ func (dbg debug_flag) Printf(format string, args ...interface{}) {
 	if dbg {
 		dbg_log.Printf(format, args...)
 	}
-}
-
-// starting point to find Yeti root servers
-var yeti_root_hints = []string{
-	"bii.dns-lab.net.",
-	"yeti-ns.wide.ad.jp.",
-	"yeti-ns.tisf.net.",
-}
-
-// maximum TTL to use
-//const MAX_TTL = 30
-//const MAX_TTL = 300
-const MAX_TTL = 86400
-
-// convert a TTL into a bounded duration
-func use_ttl(ttl uint32) time.Duration {
-	if ttl < 1 {
-		ttl = 1
-	} else if ttl > MAX_TTL {
-		ttl = MAX_TTL
-	}
-	// TODO: use normal distribution
-	result := time.Second * time.Duration(ttl+1)
-	return result
 }
 
 type ymmv_message struct {
@@ -241,277 +218,6 @@ func read_next_message() (y *ymmv_message, err error) {
 	result.answer = answer
 
 	return &result, nil
-}
-
-// information about each Yeti name server
-type ns_info struct {
-	// name of the name server, like "bii.dns-lab.net" (may be "")
-	name string
-
-	// IP addresses of the name server
-	ip []net.IP
-
-	// round-trip time (RTT) of last lookup, or 0 if none
-	last_rtt time.Duration
-
-	// time when last lookup happened, or nil if no lookup has yet occurred
-	last_lookup *time.Time
-
-	// timer for seeing if the IP address changed (may be nil)
-	timer *time.Timer
-
-	// server set we belong to
-	srvs *yeti_server_set
-}
-
-type yeti_server_set struct {
-	// prevent use of data during modification
-	lock sync.Mutex
-
-	// timer for refreshing the root zone NS RRset
-	root_ns_timer *time.Timer
-
-	// information about each IP address
-	ns []*ns_info
-
-	// algorithm used to pick server
-	algorithm string
-
-	// next server to use (for round-robin)
-	next_server int
-	next_ip     int
-
-	// resolver for lookups
-	resolver *dnsstub.StubResolver
-}
-
-// does a lookup and gets the root NS RRset
-func get_root_ns(hints []string) (ns_names []string, ns_ttl uint32) {
-	ns_query := new(dns.Msg)
-	ns_query.SetQuestion(".", dns.TypeNS)
-	ns_query.RecursionDesired = true
-
-	// Try each of our servers listed in our hints until one works.
-	// Use the rand.Perm() function so we try them in a random order.
-	var ns_response *dns.Msg
-	for _, n := range rand.Perm(len(hints)) {
-		root := hints[n]
-		if !strings.ContainsRune(root, ':') {
-			root = root + ":53"
-		}
-		var err error
-		ns_response, _, err = dnsstub.DnsQuery(root, ns_query)
-		if err != nil {
-			log.Printf("Error looking up Yeti root server NS from %s; %s", root, err)
-		}
-		if ns_response != nil {
-			break
-		}
-	}
-	if ns_response == nil {
-		log.Fatalf("Unable to get NS from any Yeti root server")
-	}
-
-	// extract out the names from the NS RRset, and also save the TTL
-	for _, root_server := range ns_response.Answer {
-		switch root_server.(type) {
-		case *dns.NS:
-			ns_names = append(ns_names, root_server.(*dns.NS).Ns)
-			ns_ttl = root_server.Header().Ttl
-		}
-	}
-
-	if len(ns_names) == 0 {
-		log.Fatalf("No NS found for Yeti root")
-	}
-
-	// insure our order is repeatable
-	sort.Strings(ns_names)
-
-	return ns_names, ns_ttl
-}
-
-func refresh_ns(srvs *yeti_server_set) {
-	dbg.Printf("refreshing root NS list")
-
-	ns_names, ns_ttl := get_root_ns(yeti_root_hints)
-
-	srvs.lock.Lock()
-	n := 0
-	for _, name := range ns_names {
-		// remove any names that are no longer present
-		for (len(srvs.ns) > n) && (srvs.ns[n].name < name) {
-			if !srvs.ns[n].timer.Stop() {
-				<-srvs.ns[n].timer.C
-			}
-			srvs.ns = append(srvs.ns[:n], srvs.ns[n+1:]...)
-		}
-		// if name is the same, we are good, advance to next name
-		if srvs.ns[n].name == name {
-			n++
-			// otherwise we have to add it
-		} else {
-			new_ns := &ns_info{name: name, srvs: srvs}
-			srvs.ns = append(srvs.ns, nil)
-			copy(srvs.ns[n+1:], srvs.ns[n:])
-			srvs.ns[n] = new_ns
-			go refresh_aaaa(new_ns, nil)
-		}
-	}
-	srvs.lock.Unlock()
-
-	// set timer to refresh our NS RRset
-	srvs.root_ns_timer = time.AfterFunc(use_ttl(ns_ttl), func() { refresh_ns(srvs) })
-}
-
-// Note that on error we don't use this name server until we can find out
-// the IP address for it. In theory we could use the name server until the
-// TTL for the address expires, but rather than track that we just
-// temporarily stop using it.
-func refresh_aaaa(ns *ns_info, done chan int) {
-	dbg.Printf("refreshing %s", ns.name)
-
-	var new_ip []net.IP
-	var this_ttl uint32
-
-	answer, _, err := ns.srvs.resolver.SyncQuery(ns.name, dns.TypeAAAA)
-	if err != nil {
-		log.Printf("Error looking up %s: %s\n", ns.name, err)
-	}
-
-	if answer != nil {
-		for _, root_address := range answer.Answer {
-			switch root_address.(type) {
-			case *dns.AAAA:
-				aaaa := root_address.(*dns.AAAA).AAAA
-				dbg.Printf("AAAA for %s = %s", ns.name, aaaa)
-				new_ip = append(new_ip, aaaa)
-				this_ttl = root_address.Header().Ttl
-			}
-		}
-	}
-
-	if len(new_ip) == 0 {
-		log.Printf("No AAAA for %s, checking again in 300 seconds", ns.name)
-		this_ttl = 300
-	}
-
-	ns.srvs.lock.Lock()
-	ns.ip = new_ip
-	when := use_ttl(this_ttl)
-	dbg.Printf("scheduling refresh of AAAA for %s in %s\n", ns.name, when)
-	ns.timer = time.AfterFunc(when, func() { refresh_aaaa(ns, nil) })
-	ns.srvs.lock.Unlock()
-
-	done <- len(new_ip)
-}
-
-// get the list of root servers from known Yeti root servers
-func yeti_priming(srvs *yeti_server_set) {
-	// get the names from the NS RRset
-	dbg.Printf("getting root NS RRset")
-	ns_names, ns_ttl := get_root_ns(yeti_root_hints)
-
-	aaaa_done := make(chan int)
-	for _, ns_name := range ns_names {
-		this_ns := &ns_info{name: ns_name, srvs: srvs}
-		// we want to complete at least one lookup before we return from priming
-		go refresh_aaaa(this_ns, aaaa_done)
-		srvs.ns = append(srvs.ns, this_ns)
-	}
-
-	found_aaaa := false
-	for _ = range ns_names {
-		num_ip := <-aaaa_done
-		if num_ip > 0 {
-			found_aaaa = true
-			break
-		}
-	}
-	if !found_aaaa {
-		log.Fatalf("No AAAA found for Yeti root NS")
-	}
-
-	// set timer to refresh our NS RRset
-	when := use_ttl(ns_ttl)
-	dbg.Printf("scheduling refresh of NS in %s\n", when)
-	srvs.root_ns_timer = time.AfterFunc(when, func() { refresh_ns(srvs) })
-}
-
-func init_yeti_server_set(ips []net.IP) (srvs *yeti_server_set) {
-	srvs = new(yeti_server_set)
-
-	if len(ips) == 0 {
-		dbg.Printf("no IP's passed, performing Yeti priming\n")
-		var err error
-		srvs.resolver, err = dnsstub.Init(4, nil)
-		if err != nil {
-			log.Fatalf("Error setting up DNS stub resolver: %s\n", err)
-		}
-		yeti_priming(srvs)
-		dbg.Printf("priming done\n")
-	} else {
-		srvs.ns = append(srvs.ns, &ns_info{ip: ips})
-	}
-
-	srvs.algorithm = "round-robin"
-	srvs.next_server = 0
-	srvs.next_ip = 0
-
-	return srvs
-}
-
-// Get the next set of IP addresses to query.
-// For most algorithms this is a single address, but it may be more (for "all").
-func (srvs *yeti_server_set) next() (ips []net.IP) {
-	srvs.lock.Lock()
-	defer srvs.lock.Unlock()
-
-	if srvs.algorithm == "round-robin" {
-		for srvs.next_ip >= len(srvs.ns[srvs.next_server].ip) {
-			srvs.next_server = (srvs.next_server + 1) % len(srvs.ns)
-			srvs.next_ip = 0
-		}
-		ips = append(ips, srvs.ns[srvs.next_server].ip[srvs.next_ip])
-		srvs.next_ip = srvs.next_ip + 1
-	} else if srvs.algorithm == "rtt" {
-		log.Fatalf("rtt-based server selection unimplemented")
-	} else {
-		var all_ips []net.IP
-		for _, ns := range srvs.ns {
-			for _, ip := range ns.ip {
-				all_ips = append(all_ips, ip)
-			}
-		}
-		if srvs.algorithm == "all" {
-			ips = all_ips
-		} else if srvs.algorithm == "random" {
-			ips = append(ips, all_ips[rand.Intn(len(all_ips))])
-		}
-	}
-	return ips
-}
-
-type yeti_server_generator struct {
-	servers *yeti_server_set
-	ips     chan []net.IP
-}
-
-func init_yeti_server_generator(algorithm string, ips []net.IP) (gen *yeti_server_generator) {
-	gen = new(yeti_server_generator)
-	gen.servers = init_yeti_server_set(ips)
-	gen.servers.algorithm = algorithm
-	gen.ips = make(chan []net.IP)
-	go func() {
-		for {
-			gen.ips <- gen.servers.next()
-		}
-	}()
-	return gen
-}
-
-func (gen *yeti_server_generator) next() (ips []net.IP) {
-	return <-gen.ips
 }
 
 // RrSort implements functions needed to sort []dns.RR
@@ -874,15 +580,63 @@ func compare_resp(iana *dns.Msg, yeti *dns.Msg) (result string) {
 	return result
 }
 
+/*
+   We want to provide the option of obfuscating the queries that we
+   are comparing, so that we don't expose the actual end-user
+   queries. However, we still want to get the same answer to the
+   query as the IANA root servers returned.
+
+   In order to do this, we take a hash of any labels that appear to
+   the left of the TLD and make a string like:
+
+       ymmv.845a838696ae1e5a.example.
+
+   We combine the labels with a value that only we know, so that an
+   observer cannot know what the original query was. (This value may
+   be set at startup, otherwise a random value is used.)
+*/
+
+var obfuscate_seed []byte
+
+func obfuscate_query(qname_in string) (qname_out string) {
+	if obfuscate_seed == nil {
+		obfuscate_seed = make([]byte, 8, 8)
+		nread, err := rand.Read(obfuscate_seed)
+		if err != nil {
+			log.Fatalf("Error generating random obfuscation seed: %s", err)
+		}
+		if nread != 8 {
+			log.Fatalf("Read %d bytes for random obfuscation seed, wanted 8", nread)
+		}
+		hex_output := make([]byte, 16, 16)
+		hex.Encode(hex_output, obfuscate_seed)
+		dbg.Printf("generated random obfuscation seed %s", strings.ToUpper(string(hex_output)))
+	}
+	labels := strings.FieldsFunc(qname_in, func(r rune) bool { return r == '.' })
+	hash_input := append(obfuscate_seed, []byte(strings.ToLower(qname_in))...)
+	hashed := sha256.Sum256(hash_input)
+	hashed_hex := make([]byte, 64, 64)
+	hex.Encode(hashed_hex, hashed[:])
+	qname_out = "ymmv." + string(hashed_hex[0:16]) + "."
+	qname_out += strings.Join(labels[len(labels)-1:len(labels)], ".") + "."
+
+	dbg.Printf("obfuscated %s to %s", qname_in, qname_out)
+	return qname_out
+}
+
 func yeti_query(gen *yeti_server_generator, iana_query *dns.Msg, iana_resp *dns.Msg, output chan string) {
 	result := ""
 	for _, ip := range gen.next() {
+		qname := obfuscate_query(iana_query.Question[0].Name)
 		server := "[" + ip.String() + "]:53"
 		result += log.Prefix()
-		result += fmt.Sprintf("Sending query '%s' %s to %s\n",
+		result += fmt.Sprintf("Sending query '%s' %s as '%s' to %s\n",
 			iana_query.Question[0].Name,
 			dns.TypeToString[iana_query.Question[0].Qtype],
+			qname,
 			server)
+		// convert to our obfuscated name
+		iana_query.Question[0].Name = qname
 		// XXX: hack hack set DO bit
 		//		iana_query.SetEdns0(4096, true)
 		//		yeti_resp, qtime, err := dnsstub.DnsQuery(server, iana_query)
@@ -911,7 +665,8 @@ func message_reader(output chan *ymmv_message) {
 
 // Main function.
 // TODO: verbose/debug flags
-// XXX: should seed the random number generator
+// TODO: turn obfuscation on/off
+// TODO: specify random seed for obfuscation
 func main() {
 	// parse any target IP specified on startup
 	var ips []net.IP
