1c3af53b12d1a7a13a68b1934730e536f84c6c20
Shane Kerr <shane@time-travellers.org>
2016-10-04 11:48:03 +0200

    go fmt

--- ymmv/yetirootns_test.go
+++ ymmv/yetirootns_test.go
@@ -2,21 +2,21 @@ package main
 
 import (
 	"testing"
-    "time"
+	"time"
 )
 
 func TestUseTTL(t *testing.T) {
 	cases := []struct {
-		ttl    uint32
-		want   time.Duration
+		ttl  uint32
+		want time.Duration
 	}{
-		{ 0, time.Second * time.Duration(0+1) },
-		{ 1, time.Second * time.Duration(1+1) },
-		{ 2, time.Second * time.Duration(2+1) },
-		{ 3600, time.Second * time.Duration(3600+1) },
-		{ 86400, time.Second * time.Duration(86400+1) },
-		{ 86401, time.Second * time.Duration(86400+1) },
-		{ 9999999, time.Second * time.Duration(86400+1) },
+		{0, time.Second * time.Duration(0+1)},
+		{1, time.Second * time.Duration(1+1)},
+		{2, time.Second * time.Duration(2+1)},
+		{3600, time.Second * time.Duration(3600+1)},
+		{86400, time.Second * time.Duration(86400+1)},
+		{86401, time.Second * time.Duration(86400+1)},
+		{9999999, time.Second * time.Duration(86400+1)},
 	}
 	for _, c := range cases {
 		got := use_ttl(c.ttl)
